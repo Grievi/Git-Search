f38a4bec5f5e085edc65f8efb1f88fd747c220df
Grievin <grievin.okumu@student.moringaschool.com>
2021-08-31 09:16:40 +0300

    add images to assets folder

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -17,17 +17,11 @@ export class ProfileService {
 
    getProfileInfo(){
      return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-    //  .pipe(
-    //   map(res => res.json())
-    
-    //  )
+ 
    }
    getProfileRepos(){
     return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-    // .pipe(
-    //  map(res => res.json())
-   
-    // )
+    
    }
 
    updateprofile( username:string){
